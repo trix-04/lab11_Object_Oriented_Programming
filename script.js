4820972b995bd19c6630c008c09697f5a044e218
trix-04 <jxt_04@outlook.com>
2024-12-13 00:25:55 -0500

    Edited JS file and created ProductProperties class.

---
+++ script.js
@@ -0,0 +1,21 @@
+//Part 1:Set up Classes
+class ProductProperties{
+    constructor(productName, price, quantity){
+        this.productName = productName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    getTotalValue(){
+        return this.price * this.quantity;
+    }
+
+    toString(){
+        return (`Product: ${this.productName}, Price: ${this.price}, Quantity: ${this.quantity}`)
+    }
+
+}
+
+const newItem = new ProductProperties("Apples", 12, 5);
+console.log(newItem.getTotalValue());
+console.log(newItem.toString());
